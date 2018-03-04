b3978b808866e7de18e99bf767c21dfc49af0089
Carlos Caro Álvarez <carloscaroalvarez@gmail.com>
2018-03-04 20:03:05 +0100

    0.5

---
+++ cmds.js
@@ -0,0 +1,135 @@
+"use strict";
+
+const colorize = require('./out').colorize;
+const log = require('./out').log;
+const biglog = require('./out').biglog;
+const errorlog = require('./out').errorlog;
+
+
+const model = require('./model');
+
+
+
+/**
+ * Muestra la ayuda.
+ * @param rl Objeto readLine usado para implementar el CLI
+ */
+exports.helpCmd = rl => {
+    log("Comandos:");
+    log(" h|help - Muestra esta ayuda.");
+    log(" list - Listear los quizzes existentes.");
+    log(" show <id> - muestra la pregunta y la respuesta.");
+    log(" add - Añadir un nuevo quizz.");
+    log(" delete <id> - Borrar el quizz indicado.");
+    log(" edit <id> - Editar el quizz indicado.");
+    log(" test <id> - Probar el quizz indicado.");
+    log(" p|play - Jugar a preguntar aleatoriamente todos los quizzes.");
+    log(" credits - Créditos.");
+    log(" q|quit - Salir del programa.");
+    rl.prompt();
+};
+/**
+ * Lista todos los quizzes existentes en el modelo.
+ */
+exports.listCmd = rl => {
+    model.getAll().forEach(((quiz,id) => { 
+        log(`[${colorize(id,'magenta')}]:${quiz.question}`);
+    }));
+    //log('Listar todos los quizzes existentes.','red');
+    rl.prompt();
+};
+/**
+ * Muestra el quiz indicado en el parámetro: la pregunta y la respuesta.
+ * @param id clave del quiz a mostrar.
+ */
+exports.showCmd = (rl, id) => {
+    if (typeof id === "undefined"){
+        errorlog(`Falta el parámetro id.`);
+    }else{
+        try{
+            const quiz = model.getByIndex(id);
+            log(`[${colorize(id,'magenta')}]:${quiz.question} ${colorize('=>','magenta')} ${quiz.answer}`);
+        }catch(error){
+            errorlog(error.message);
+        }
+    }
+    //log('Mostrar el quiz indicado.','red');
+    rl.prompt();
+};
+/**
+ * Añade un nuevo quiz al modelo.
+ * @param id Clave del quiz a mostrar.
+ */
+exports.addCmd = rl => {
+    log('Añadir un nuevo quiz.','red');
+    rl.prompt();
+};
+/**
+ * Prueba el quizz, es decir, hace una pregunta del modelo a la que debemos contestar.
+ * @param id clave del quiz a probar.
+ */
+exports.testCmd = (rl, id) => {
+    log('Probar el quiz indicado.','red');
+    rl.prompt();
+};
+/**
+ * Borra un quizz del modelo.
+ * @param id Clave del quiz a borrar en el modelo.
+ */
+exports.deleteCmd = (rl, id) => {
+    log('Borrar el quiz indicado.','red');
+    rl.prompt();
+};
+/**
+ * Edita un quiz del modelo.
+ * @param id Clave del quiz a editar en el modelo.
+ */
+exports.editCmd = (rl,id) => {
+    log('Editar el quiz indicado.','red');
+    rl.prompt();
+};
+
+/**
+ * Pregunta todos los quizzes existentes en el modleo en orden alfabético.
+ * Se gana si contesta a todos satisfactoriamente.
+ */
+exports.playCmd = rl => {
+    log('Jugar.','red');
+    rl.prompt();
+};
+/**
+ * Muestra los nombres de los autores de la práctica.
+ *
+ */
+exports.creditsCmd = rl => {
+    log('Autores de la práctica:');
+    log("\n Marta Hernández Muela\n&\n Carlos Caro Álvarez",'green');
+    rl.prompt();
+};
+/**
+ * Termina el programa.
+ *
+ */
+exports.quitCmd = rl => {
+    log("¡Adiós!",'blue');
+    rl.close();
+
+};
+/**
+ * Mensaje por defecto cuando se da una orden desconocida.
+ *
+ */
+/*exports.defaultCmd = () => {
+    console.log(`\n¿${colorize(cmd,"red")}?\nCreo que te he entendido mal.\nPor favor introduce un comando válido.\nEjecute el comando ${colorize("help","green")}...\n`);
+    rl.prompt();
+};
+ */
+/**
+ * Mensaje por defecto si eres tentado por el Lado Oscuro de la Fuerza
+ *
+ */
+exports.senateCmd = rl => {
+    log("I thought not. It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life... He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful... the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. It's ironic; he could save others from death, but not himself.",'red'
+    );
+    rl.prompt();
+};
--- main.js
+++ main.js
@@ -1,54 +1,13 @@
-
+"use strict";
 const readline = require('readline');
+const model = require('./model');
+const cmds = require('./cmds');
+const colorize = require('./out').colorize;
+const log = require('./out').log;
+const biglog = require('./out').biglog;
+const errorlog = require('./out').errorlog;
 
-const figlet = require('figlet');
-const chalk = require('chalk');
-
-
-/**
- * Da color a un string
- *
- * @param msg           El String al que hay que dar color.
- * @param color         Color con el que pintar msg.
- * @returns {string}    Devuelve el string msg con el color indicado.
- */
-const colorize = (msg, color) => {
-    if (typeof color !== "undefined"){
-        msg = chalk[color].bold(msg);
-    }
-    return msg;
-};
-
-/**
- * Escribe un mensaje de log.
- *
- * @param msg           El String a escribir
- * @param color         Color del texto.
- */
-const log = (msg, color) => {
-    console.log(colorize(msg,color));
-};
-
-/**
- * Escribe un mensaje de log grande.
- *
- * @param msg           Texto a escribir
- * @param color         Color del texto.
- */
-const biglog = (msg, color) => {
-    log(figlet.textSync(msg, { horizontalLayout: 'full' }), color);
-};
-
-/**
- * Escribe un mensaje de error emsg.
- *
- * @param emsg          Texto del mensaje de error.
- */
-const errorlog = (emsg) => {
-    console.log(`${colorize("Error", "red")}: &{colorize(colorize(emsg, "red"), "bgYellowBright")}`);
-};
-
-
+//import {colorize, log, biglog, errorlog} from 'out';
 
 
 //Mensaje inicial
@@ -57,174 +16,86 @@ biglog('CORE Quiz','green');
 
 
 const rl = readline.createInterface({
-                                    input: process.stdin,
-                                    output: process.stdout,
-                                    prompt: 'quiz> ',
-                                    completer:(line) => {
-                                    const completions = 'h help add delete edit list test p play credits q quit'.split(' ');
-                                    const hits = completions.filter((c) => { return c.indexOf(line) === 0 });
-                                    // show all completions if none found
-                                    return [hits.length ? hits : completions, line];
-                                    }
-                                    });
+        input: process.stdin,
+        output: process.stdout,
+        prompt: colorize('quiz > ','blue'),
+        completer: (line) => {
+            const completions = 'h help add delete edit list test p play credits q quit'.split(' ');
+            const hits = completions.filter((c) => { return c.indexOf(line) === 0 });
+            // show all completions if none found
+            return [hits.length ? hits : completions, line];
+        }
+    }
+);
+
+rl.setPrompt(colorize('quiz > ','blue'));
 
 rl.prompt();
 
 rl
 .on('line', (line) => {
-    "use strict";
+    //"use strict";
     let args = line.split(" ");
     let cmd = args[0].toLowerCase().trim();
-    
       switch (cmd) {
       case '':
         rl.prompt();
         break;
       default:
-            defaultCmd();
+    //defaultCmd();
+        console.log(`\n¿${colorize(cmd,"red")}?\nCreo que te he entendido mal.\nPor favor introduce un comando válido.\nEjecute el comando ${colorize("help","green")}...\n`);
+        rl.prompt();
+        break;
       case 'h':
       case 'help':
-            helpCmd();
+            cmds.helpCmd(rl);
         break;
       case 'q':
       case'quit':
-            quitCmd();
+            cmds.quitCmd(rl);
         break;
       case 'p':
       case 'play':
-      playCmd();
+            cmds.playCmd(rl);
         break;
       case 'add':
-            addCmd();
+            cmds.addCmd(rl);
         break;
       case 'list':
-            listCmd();
+            cmds.listCmd(rl);
         break;
       case 'show':
-            showCmd(args[1]);
+            cmds.showCmd(rl,args[1]);
         break;
       case 'hello':
             console.log("it's me");
         break;
       case 'test':
-            testCmd(args[1]);
+            cmds.testCmd(rl,args[1]);
         break;
       case 'delete':
-            deleteCmd(args[1]);
+            cmds.deleteCmd(rl,args[1]);
         break;
       case 'edit':
-            editCmd(args[1]);
+            cmds.editCmd(rl,args[1]);
         break;
       case 'credits':
-            creditsCmd();
+            cmds.creditsCmd(rl);
+        break;
+    case 'tragedy':
+            cmds.senateCmd(rl);
         break;
       /*default:
       console.log(`Say what? I might have heard '${line.trim()}'`);
-      break;*/
+      break;
       }
       })
 .on('close', () => {
-            console.log('¡Adiós!');
+            log('¡Adiós!','blue');
             process.exit(0);
             });
+*/
 
-/**
- * Muestra la ayuda.
- */
-const helpCmd = () => {
-    console.log("Comandos:");
-    console.log(" h|help - Muestra esta ayuda.");
-    console.log(" list - Listear los quizzes existentes.");
-    console.log(" show <id> - muestra la pregunta y la respuesta.");
-    console.log(" add - Añadir un nuevo quizz.");
-    console.log(" delete <id> - Borrar el quizz indicado.");
-    console.log(" edit <id> - Editar el quizz indicado.");
-    console.log(" test <id> - Probar el quizz indicado.");
-    console.log(" p|play - Jugar a preguntar aleatoriamente todos los quizzes.");
-    console.log(" credits - Créditos.");
-    console.log(" q|quit - Salir del programa.");
-    rl.prompt();
-};
-/**
- * Lista todos los quizzes existentes en el modelo.
- */
-const listCmd = () => {
-    console.log('Listar todos los quizzes existentes.');
-    rl.prompt();
-};
-/**
- * Muestra el quiz indicado en el parámetro: la pregunta y la respuesta.
- * @param id clave del quiz a mostrar.
- */
-const showCmd = id => {
-    console.log('Mostrar el quiz indicado.');
-    rl.prompt();
-};
-/**
- * Añade un nuevo quiz al modelo.
- * @param id Clave del quiz a mostrar.
- */
-const addCmd = () => {
-    console.log('Añadir un nuevo quiz.');
-    rl.prompt();
-};
-/**
- * Prueba el quizz, es decir, hace una pregunta del modelo a la que debemos contestar.
- * @param id clave del quiz a probar.
- */
-const testCmd = id => {
-    console.log('Probar el quiz indicado.');
-    rl.prompt();
-};
-/**
- * Borra un quizz del modelo.
- * @param id Clave del quiz a borrar en el modelo.
- */
-const deleteCmd = id => {
-    console.log('Borrar el quiz indicado.');
-    rl.prompt();
-};
-/**
- * Edita un quiz del modelo.
- * @param id Clave del quiz a editar en el modelo.
- */
-const editCmd = id => {
-    console.log('Editar el quiz indicado.');
-    rl.prompt();
-};
-
-/**
- * Pregunta todos los quizzes existentes en el modleo en orden alfabético.
- * Se gana si contesta a todos satisfactoriamente.
- */
-const playCmd = () => {
-    console.log('Jugar.');
-    rl.prompt();
-};
-/**
- * Muestra los nombres de los autores de la práctica.
- *
- */
-const creditsCmd = () => {
-    console.log('Autores de la práctica:\n Marta Hernández Muela\n&\n Carlos Caro Álvarez');
-    rl.prompt();
-};
-/**
- * Termina el programa.
- *
- */
-const quitCmd = () => {
-    rl.close();
-    rl.prompt();
-};
-/**
- * Mensaje por defecto cuando se da una orden desconocida.
- *
- */
-const defaultCmd = () => {
-    console.log(`\n¿'${cmd}'?\nCreo que te he entendido mal.\nPor favor introduce un comando válido.\nEjecutando comando help...\n`);
-    //rl.prompt();
-};
-
-
-
+        }
+    }
+)
